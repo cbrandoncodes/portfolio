72f7e54fd9f5c740223ef93863ff427a1fa203ad
Brandon Chikezie <75492569+Brandon30067308@users.noreply.github.com>
2024-10-21 17:20:23 +0100

    chore: update robots.txt

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export const themeStoreKey = "brandon-web-theme";
 
 export const appUrl =
-  process.env.NEXT_PUBLIC_APP_URL ?? "https://www.brandondev.io";
+  process.env.NEXT_PUBLIC_APP_URL ?? "https://www.brandondev.site";
