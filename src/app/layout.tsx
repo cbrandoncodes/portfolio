818d5e31fc275359ac4bde310b64c69018653cb7
Brandon Chikezie <75492569+Brandon30067308@users.noreply.github.com>
2025-09-20 19:09:32 +0100

    feat: add manifest.json and update metadata configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,26 @@ export default function RootLayout({
 }) {
   return (
     <html data-theme="light" lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         className={clsx(
           "custom-scroll",
